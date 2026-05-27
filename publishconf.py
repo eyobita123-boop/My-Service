9047974467ee0debafe62137d170f00cc9d247d2
Eyob Dereje Bekele <eyobita123@gmail.com>
2026-05-27 22:57:58 +0300

    Update SITEURL to include My-service path

--- publishconf.py
+++ publishconf.py
@@ -3,6 +3,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://eyobita123-boop.github.io'
+SITEURL = 'https://eyobita123-boop.github.io/My-service'
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
