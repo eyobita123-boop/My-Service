2b30aaf4c7198e7345234a3e46173698d26b2f13
Eyob Dereje Bekele <eyobita123@gmail.com>
2026-05-27 22:12:09 +0300

    Add initial Pelican configuration settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,1 +1,25 @@
+AUTHOR = 'Eyob Dereje Bekele'
+SITENAME = 'Eyob Dereje Bekele | Financial & Digital Services'
+SITEURL = ''
 
+PATH = 'content'
+TIMEZONE = 'Africa/Addis_Ababa'
+DEFAULT_LANG = 'en'
+
+# Theme
+THEME = 'theme/eyob-theme'
+
+# Use the custom home template for any page with slug 'home'
+TEMPLATE_PAGES = {
+    'pages/home.html': 'index.html',   # this makes the home page our custom template
+}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blog settings (keep for future)
+DEFAULT_PAGINATION = False
