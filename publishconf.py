88d48da111043d2c9bed894a15562c34cde5384f
Eyob Dereje Bekele <eyobita123@gmail.com>
2026-05-27 22:52:10 +0300

    Fix SITEURL formatting in publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -3,6 +3,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://<eyobita123-boop>.github.io'
+SITEURL = 'https://eyobita123-boop.github.io'
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
