8d0d3a28b11819377220c7e6e9f339f5e956cb0a
Eyob Dereje Bekele <eyobita123@gmail.com>
2026-05-27 22:15:56 +0300

    Configure publish settings for deployment

--- publishconf.py
+++ publishconf.py
@@ -1,1 +1,8 @@
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
 
+SITEURL = 'https://<eyobita123-boop>.github.io'
+RELATIVE_URLS = False
+DELETE_OUTPUT_DIRECTORY = True
