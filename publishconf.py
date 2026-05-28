fb442a3a859c022e68b8ec398a1062fc5dab37da
Eyob Dereje Bekele <eyobita123@gmail.com>
2026-05-28 19:50:32 +0300

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -3,6 +3,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://eyobita123-boop.github.io/My-service'
+SITEURL = 'https://my-service-aer.pages.dev/'
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
